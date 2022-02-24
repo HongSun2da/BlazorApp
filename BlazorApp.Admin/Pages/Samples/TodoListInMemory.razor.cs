548c09112f4d05f1bfa1e8c58a10bca6644ff024
hongsun2da <hongsun2da@gmail.com>
2022-02-24 16:46:59 +0900

    인메모리 동작 원리 확인

---
+++ BlazorApp.Admin/Pages/Samples/TodoListInMemory.razor.cs
@@ -0,0 +1,27 @@
+﻿using BlazorApp.Models;
+
+namespace BlazorApp.Admin.Pages.Samples
+{
+    public partial class TodoListInMemory
+    {
+        private string newTodo = "";
+        private IList<TodoItem> todos = new List<TodoItem>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            todos.Add(new TodoItem() { Id = 1, Title = "할일1", IsDone = true });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected void Save()
+        {
+            todos.Add(new TodoItem() { Title = newTodo });
+            newTodo = null;
+        }
+    }
+}
