b01286e5619106a4c490f559d53e8704d23a8720
hongsun2da <hongsun2da@gmail.com>
2022-02-24 17:51:08 +0900

    Model생성 및 Compoents 제작하기

---
+++ BlazorApp/Components/SiteList.razor.cs
@@ -0,0 +1,25 @@
+﻿using BlazorApp.Models;
+
+namespace BlazorApp.Components
+{
+    public partial class SiteList
+    {
+        private List<Site> sites = new List<Site>();
+
+        public List<Site> Sites
+        {
+            get { return sites; }
+            set { sites = value; }
+        }
+
+        protected override void OnInitialized()
+        {
+            sites = new List<Site>() {
+                new Site{ Id=1, Url="https://azure.microsoft.com", Name="Azure Site", Description="Microsoft Azure: 클라우드 컴퓨팅 서비스"},
+                new Site{ Id=2, Url="https://blazor.net", Name="Blazor Site", Description="Blazor | Build client web apps with C# | .NET - Microsoft"},
+                new Site{ Id=3, Url="https://docs.microsoft.com", Name="C# Site", Description="C# 문서 - 시작, 자습서, 참조."},
+                new Site{ Id=4, Url="https://dotnet.microsoft.com", Name="Dotnet Site", Description="NET | Free. Cross-platform. Open Source"}
+            };
+        }
+    }
+}
---
+++ BlazorApp/Components/TechList.razor.cs
@@ -0,0 +1,25 @@
+﻿using BlazorApp.Models;
+
+namespace BlazorApp.Components
+{
+    public partial class TechList
+    {
+        private List<Tech> teches = new List<Tech>();
+
+        public List<Tech> Teches
+        {
+            get { return teches; }
+            set { teches = value; }
+        }
+
+        protected override void OnInitialized()
+        {
+            teches = new List<Tech>() {
+                new Tech{ Id=1, Name="Azure", CreateDate=DateTime.Now},
+                new Tech{ Id=2, Name="Blazor", CreateDate=DateTime.Now},
+                new Tech{ Id=3, Name="C#", CreateDate=DateTime.Now},
+                new Tech{ Id=4, Name="Dotnet", CreateDate=DateTime.Now}
+            };
+        }
+    }
+}
