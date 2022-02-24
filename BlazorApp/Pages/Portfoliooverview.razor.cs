5965704cff16ae24529aded662f9eecc37f5d80a
hongsun2da <hongsun2da@gmail.com>
2022-02-24 10:35:09 +0900

    코드에서 페이지 이동 NavigationManager

---
+++ BlazorApp/Pages/Portfoliooverview.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Pages
+{
+    public partial class Portfoliooverview
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+            
+        }
+
+        protected void GoToNaver()
+        {
+            NavigationManager.NavigateTo("https://www.naver.com");
+        }
+    }
+}
